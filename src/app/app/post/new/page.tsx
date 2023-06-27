32d5b1d317257dc21424cf598e7f1a0e50a55a53
Andriy Bas <andriy.bas@uptech.team>
2023-06-27 21:41:48 +0300

    some styles

--- src/app/app/post/new/page.tsx
+++ src/app/app/post/new/page.tsx
@@ -2,6 +2,8 @@
 import * as React from "react";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { useRouter, redirect } from "next/navigation";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBrain } from "@fortawesome/free-solid-svg-icons";
 
 interface Post {
   postContent: string;
@@ -11,6 +13,7 @@ interface Post {
 
 const NewPostPage: React.FC = () => {
   const { user, isLoading } = useUser();
+  const [isGenerating, setIsGenerating] = React.useState(false);
   const router = useRouter();
   if (isLoading) {
     return <p>Loading...</p>;
@@ -27,49 +30,75 @@ const NewPostPage: React.FC = () => {
       els.namedItem("keywords") as HTMLTextAreaElement
     ).value.trim();
 
-    const response = await fetch("/api/generate-post", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ topic, keywords, uid: user.sub }),
-    });
-    const json = await response.json();
-    console.log("json: ", json);
-    if (json?.post?._id) {
-      router.push(`/app/post/${json.post._id}`);
+    setIsGenerating(true);
+    try {
+      const response = await fetch("/api/generate-post", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ topic, keywords, uid: user.sub }),
+      });
+      const json = await response.json();
+      console.log("json: ", json);
+      if (json?.post?._id) {
+        router.push(`/app/post/${json.post._id}`);
+      }
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setIsGenerating(false);
     }
   };
 
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <div>
-          <label htmlFor="topic">
-            <b>Generate a blog post on the topic of:</b>
-          </label>
-          <textarea
-            className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
-            defaultValue="Top 10 tips for dog owners"
-            name="topic"
-            id="topic"
+    <div className="min-h-screen overflow-hidden">
+      {isGenerating && (
+        <div className="text-green-500 flex w-full h-full flex-col animate-pulse items-center justify-center">
+          <FontAwesomeIcon
+            icon={faBrain}
+            width={100}
+            height={100}
+            className="text-6xl"
           />
+          <h6>Generating...</h6>
         </div>
-        <div>
-          <label htmlFor="keywords">
-            <b>Targetting the following keywords:</b>
-          </label>
-          <textarea
-            className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
-            id="keywords"
-            defaultValue="first-time dog owners, common dog health issues, best dog breeds"
-            name="keywords"
-          />
+      )}
+      {!isGenerating && (
+        <div className="flex w-full h-full flex-col overflow-auto">
+          <form
+            onSubmit={onSubmit}
+            className="m-auto w-full max-w-screen-sm bg-slate-100 p-4 rounded-md shadow-xl border border-slate-200 shadow-slate-200"
+          >
+            <div>
+              <label htmlFor="topic">
+                <b>Generate a blog post on the topic of:</b>
+              </label>
+              <textarea
+                className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+                defaultValue="Top 10 tips for dog owners"
+                name="topic"
+                id="topic"
+              />
+            </div>
+            <div>
+              <label htmlFor="keywords">
+                <b>Targetting the following keywords:</b>
+              </label>
+              <textarea
+                className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+                id="keywords"
+                defaultValue="first-time dog owners, common dog health issues, best dog breeds"
+                name="keywords"
+              />
+            </div>
+            <small className="block mb-2">Separate keywords with comma</small>
+            <button type="submit" className="btn">
+              Generate
+            </button>
+          </form>
         </div>
-        <button type="submit" className="btn">
-          Generate
-        </button>
-      </form>
+      )}
     </div>
   );
 };
